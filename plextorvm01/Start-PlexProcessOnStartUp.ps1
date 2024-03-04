299b54ec5c3c167472cfe0b2a97598e553726c73
joedel94 <joedel94@gmail.com>
2024-03-03 22:17:04 -0500

    Updated help text and removed unneeded ComputerName param

--- plextorvm01/Start-PlexProcessOnStartUp.ps1
+++ plextorvm01/Start-PlexProcessOnStartUp.ps1
@@ -1,27 +1,25 @@
 <#
 .SYNOPSIS
+    Start Plex on homelab VM
 
 .DESCRIPTION
-    This script is designecd to be run at initial start up 
-
-.PARAMETER ComputerName
+    This script is designecd to be run at initial start up on plextorvm01 and 
+    will start the  Plex Media Server process.
 
 .PARAMETER Path
-    defaults to the current path on plextorvm01
+    Path of plex server .exe, defaults to the current path on plextorvm01
 
 .NOTES
-    Written by Joe Delaney - 2/28/2024... its my first script on my personal repo.... this is so long overdue
+    Written by Joe Delaney - 2/28/2024... its my first script on my personal 
+    repo.... this is so long overdue
 
 .LINK
     ObnoxiousJoe github repo: https://github.com/joedel94/ObnoxiousJoe
-
 #>
 #region Parameters
 
     [CmdletBinding()]
     Param (
-        [Parameter(Mandatory = $false)]
-        [string]$ComputerName = 'plextorvm01',
         [Parmeter(Mandatory = $false)]
         [string]$Path = 'C:\Program Files (x86)\Plex\Plex Media Server'
     )
@@ -50,7 +48,8 @@
     $currentProcs = Get-Process 
     if('Plex Media Server' -in $currentProcs.ProcessName) {
         #using write-host since my VM is on Windows Server 2012 R2.... dumb, Future Joe FIX THAT!
-        Write-Host 'Plex server .exe '
+        #TODO update this to Write-Information when I upgrade this VM to user PS 7 and Windows Server 2016+
+        Write-Host 'Plex server .exe started successfully'
         return
     }
     else {
